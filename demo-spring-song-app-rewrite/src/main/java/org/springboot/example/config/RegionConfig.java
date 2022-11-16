f9cdb700aa6d41f32497d84542ffda9626724903
Andrei Shakirin <andrei.shakirin@gmail.com>
2022-11-16 18:50:28 +0100

    Added test demo project

--- demo-spring-song-app-rewrite/src/main/java/org/springboot/example/config/RegionConfig.java
+++ demo-spring-song-app-rewrite/src/main/java/org/springboot/example/config/RegionConfig.java
@@ -1,13 +1,13 @@
-/*~~(Recipe applicable test failed with an exception.
-java.lang.IllegalAccessError: class org.openrewrite.java.testing.junit5.UpdateTestAnnotation tried to access protected method 'org.openrewrite.java.JavaIsoVisitor org.openrewrite.java.search.FindImports.getVisitor()' (org.openrewrite.java.testing.junit5.UpdateTestAnnotation and org.openrewrite.java.search.FindImports are in unnamed module of loader org.codehaus.plexus.classworlds.realm.ClassRealm @5f726750)
-  org.openrewrite.java.testing.junit5.UpdateTestAnnotation.getSingleSourceApplicableTest(UpdateTestAnnotation.java:49))~~>*/package org.springboot.example.config;
+package org.springboot.example.config;
 
 import lombok.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
 
 import java.util.Locale;
 
 @Value
+@ConstructorBinding
 @ConfigurationProperties(prefix = "region")
 public class RegionConfig {
     private Locale regionCode;
--- demo-spring-song-app-rewrite/src/main/java/org/springboot/example/songs/persistence/SongStatRepository.java
+++ demo-spring-song-app-rewrite/src/main/java/org/springboot/example/songs/persistence/SongStatRepository.java
@@ -14,9 +14,7 @@
  * limitations under the License.
  */
 
-/*~~(Recipe applicable test failed with an exception.
-java.lang.IllegalAccessError: class org.openrewrite.java.testing.junit5.UpdateTestAnnotation tried to access protected method 'org.openrewrite.java.JavaIsoVisitor org.openrewrite.java.search.FindImports.getVisitor()' (org.openrewrite.java.testing.junit5.UpdateTestAnnotation and org.openrewrite.java.search.FindImports are in unnamed module of loader org.codehaus.plexus.classworlds.realm.ClassRealm @5f726750)
-  org.openrewrite.java.testing.junit5.UpdateTestAnnotation.getSingleSourceApplicableTest(UpdateTestAnnotation.java:49))~~>*/package org.springboot.example.songs.persistence;
+package org.springboot.example.songs.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
